9976588d12190e3579439fec840926181ec182ef
Michael Sanders <michael.sanders@heliosinteractive.com>
2019-02-01 11:07:53 -0800

    Moved template code to separate file.

--- index.js
+++ index.js
@@ -11,39 +11,15 @@
  *    momo-the-monster
  */
 
+var fs = require('fs');
+var path = require('path');
+ 
+var template = fs.readFileSync(path.join(__dirname,'template.html')).toString();
+ 
 module.exports = function(corsica) {
-  
-  // use minimal embedding template from Vimeo
-  var template = `<iframe src='https://player.vimeo.com/video/{{id}}?muted=1&autoplay=1&loop={{loop}}&title=0&byline=0&portrait=0' 
-                            style='position:absolute;top:0;left:0;width:100%;height:100%;' 
-                            frameborder='0' 
-                            webkitallowfullscreen 
-                            mozallowfullscreen 
-                            allowfullscreen></iframe>       
-                    <script src='https://player.vimeo.com/api/player.js'></script>
-                    <script>
-                        var iframe = document.querySelector('iframe');
-                        var player = new Vimeo.Player(iframe);
-
-                        //*************************************************************
-                        //This section of code strips out the 'blob' portion of the url
-                        var url = window.location.href;
-                        url = url.substring(5);
-                        var index = url.indexOf("/");
-                        index = url.indexOf("/", index + 2);
-                        url = url.substring(0, index);
-                        url += "/reset?id={{name}}";
-                        //*************************************************************
-
-                        player.on('ended', function() {
-                            var req = new XMLHttpRequest();
-                            req.open("GET", url, true);
-                            req.send(null);
-                        });
-                    </script>`;
-
+	
   // Transform vimeo entries into embeds using id property
-  //TODO: transform Vimeo URLs too
+  // TODO: transform Vimeo URLs too
   corsica.on('vimeo', function(content) {
     if (content.id === undefined) return;
 	
